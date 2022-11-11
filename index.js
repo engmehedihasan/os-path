369e9ba8a409823602bae007adc3d807591de2b7
Md Mehedi Hasan <116740305+engmehedihasan@users.noreply.github.com>
2022-11-11 19:39:11 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const os = require("os");
+
+ //console.log(os.userInfo());
+
+// console.log(os.hostname());
+
+ //console.log(os.totalmem());
+
+ //console.log(os.freemem());
+
+ //console.log(__dirname);
+
+ const path = require("path");
+
+ console.log(path);
+
+  
